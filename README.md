31b076d4cce2066b53d6cc5508e12d3d433df3ca
mgcvale <miguelcvalealt@gmail.com>
2024-05-27 19:12:53 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,72 @@
+# tjmg-scraper
+Um scraper básico para coleta de inteiros teores do Tribunal de Justica de Minas Gerais (TJMG)
+
+# uso do scraper
+
+A documentacão das funcões oferecidas pelo scraper estão no código fonte, mas aqui está uma lista geral delas:
+
+get_nums_processuais: faz a coleta de todos os números processuais de uma busca avancada de jurisprudencia em segunda instância no TJMG.
+
+get_processo_table: faz o download de diversas informacões de uma lista de números processuais. A lista das informacões a serem coletadas, em ordem, está na documentacao do código. A funcao retorna tanto um array do python com as infos, quando faz insert em um banco de dados.
+
+get_inteiro_teor: faz o download do inteiro teor (ou acórdão) de um processo pelo seu número processual em pdf, no diretório informado.
+
+normalize_tjmg_dataset: Faz a formatacão de acórdãos não formatadas, separando-as em acórdão, ementa, e súmula.
+
+
+Exemplo:
+
+```python
+import mysql.connector as connector
+import src.number_scraper as tjmg
+import json
+
+# pegar os números processuais de uma pesquisa sobre apelacao criminal
+numeros = tjmg.get_num_processuais("Apelacao Cível", "9", "08%2F04%2F2023", "08%2F05%2F2023")
+# salvar tudo em um arquivo de texto
+with open("apelacao criminal/numprocs.txt", 'w') as file:
+    file.write("\n".join(numeros))
+
+# abrir o arquivo que acabamos de fazer com todos os números de processo
+with open("apelacao criminal/numprocs.txt", "r") as file:
+    entries = file.readlines()
+entries = [entry.strip() for entry in entries]
+
+# criar uma conexão no MySql
+host = '111.222.333.4'
+user = 'nome_usuario'
+password = 'senha_usuario'
+database = 'nome_db'
+connection = connector.connect(host=host, user=user, password=password, port=3306, database=database)
+cursor = connection.cursor()
+
+# pegar a tabela de dados raspada pelo scraper. Vamos salvar tanto no banco de dados, passando a conexao e o cursor, 
+# tanto em um array do python, setando o returns como True.
+tabela = tjmg.get_processo_table(numprocs=entries, connection=connection, cursor=cursor, returns=True)
+
+# fechar conexão com o banco de dados
+if 'cursor' in locals():
+    cursor.close()
+if 'connection' in locals() and connection.is_connected():
+    connection.close()
+
+# transformar a tabela que pegamos em um arquivo json
+with open("/apelacao criminal/processo.json", 'wb'):
+    json.dump(tabela)
+```
+
+Nesse exemplo, fazemos a raspagem dos números processuais de uma busca sobre Apelacao Criminal, com cerca de 4500 processos, guardamos estes processos em um arquivo, e fazemos a raspagem e o download de todas as informacoes destes processos, guardando-as tanto em um banco de dados, quanto em um arquivo do tipo "json".
+
+# dependências
+
+a lista completa de requerimentos está no arquivo requirements.txt, mas as principais bibliotecas que você deverá ter são:
+
+mysql
+
+fitz (PyMuPDF)
+
+SpeechRecognition
+
+selenium
+
+requests
---
+++ src/process_scraper.py
@@ -0,0 +1,192 @@
+from os import getcwd
+from time import sleep
+import re
+
+import mysql
+import requests
+from fitz import fitz
+from src import number_scraper as ns
+
+
+def remove_first_line(filepath):
+    with open(filepath, 'r') as file:
+        lines = file.readlines()[1:]
+    with open(filepath, 'w') as file:
+        file.writelines(lines)
+
+
+def get_inteiro_teor(numproc: str, dir = getcwd() + "/inteiros-teores", timeout=3, filename = None):
+
+    # formatar o número processual
+    parts = ns.get_numproc_numbers(numproc)
+    print(parts)
+
+    #  pegar o url do inteiro teor
+    url = ("https://www5.tjmg.jus.br/jurisprudencia/relatorioEspelhoAcordao.do?inteiroTeor=true&ano="
+           + parts[2] + "&ttriCodigo=" + parts[0] + "&codigoOrigem=" + parts[1] + "&numero=" + parts[3] +
+           "&sequencial=" + parts[5] + "&sequencialAcordao=0")
+
+    # setup do nome do arquivo e diretório
+    if(filename is None):
+        dir = dir + "/"
+        for string in parts:
+            dir = dir + string
+        dir = dir + ".pdf"
+    else:
+        dir = dir + "/" +  filename + ".pdf"
+
+    # debug
+    print(dir)
+    print(url)
+
+    # fazer requisicão e pegar o seu resultado
+    try:
+        r = requests.get(url, allow_redirects=True, timeout=timeout)
+        print(r)
+    except:
+        print("nao foi possivel fazer a requisicao.")
+        raise Exception("request_error")
+    open(dir, "wb").write(r.content)
+    sleep(0.05)
+
+
+def get_data_from_numproc(numproc, dir):
+    data = [numproc]
+
+    # fazer download do pdf do acórdão e o transformar em texto
+    try:
+        get_inteiro_teor(numproc, dir + "/temp", filename="acordao")
+    except:
+        return None, None
+
+    acordao_txt = ''
+    try:
+        acordao_doc = fitz.open(dir + "/temp/acordao.pdf")
+    except:
+        return None, None
+    for pagen in range(acordao_doc.page_count):
+        page = acordao_doc.load_page(pagen)
+        acordao_txt += page.get_text()
+
+    # pegar a ementa e súmula do acórdão
+    pattern_list = [r'^.*?EMENTA:', r'(?<=\n)\d+(?=\n)', r'Tribunal de Justiça de Minas Gerais\n']
+
+    data_pdf = re.sub('|'.join(pattern_list), '', acordao_txt, flags=re.DOTALL)
+
+    ementa = re.sub(r'(.*?)A\s+C\s+Ó\s+R\s+D\s+Ã\s+O.*', r'\1', data_pdf, flags=re.DOTALL)
+    split = ementa.split('\n\n')
+    ementa = ' '.join(split[:-1]) if len(split) != 1 else ementa.join(split)
+    ementa = re.sub(r'\s+', ' ', ementa)
+
+    acordao = re.sub(r'.*?V\sO\sT\sO\s+(.*?)SÚMULA.*', r'\1', data_pdf, flags=re.DOTALL)
+    acordao = re.sub(r'\s+', ' ', acordao)
+
+    sumula = re.sub(r'.*?SÚMULA:+(.*?)', r'\1', data_pdf, flags=re.DOTALL)
+    sumula = re.sub(r'\s+', ' ', sumula)
+
+    # concatenar o resto das informacões
+    data.append(acordao)
+    data.append(ementa)
+    data.append(sumula)
+
+    return data, acordao
+
+
+def get_processo_table_essentials(numprocs, dir = getcwd() + "/processos", connection=None, cursor=None, returns = True, lowerbound: int = 700*5.5, upperbound: int = 4000*5.5, delay=0.15):
+    """
+    Faz a raspagem dos dados essenciais de uma lista de processos, retornando uma tabela (em forma de array OU em um banco de
+    dados) com o número processual, acordao formatado, ementa formatada e sumula
+    formatada.
+
+    :param numprocs: str []:  lista dos números processuais a serem consultados
+    :param dir: str: diretório para salvar dados temporários utilizados pelo scraper
+    :param connection: connection: conexão do banco de dados no qual as informacoes serão armazenadas.
+    :param cursor: cursor: cursor com o banco de dados no qual as informacões serão armazenadas.
+    :param returns: bool: flag indicando se a funcao deverá retornar um array com as informacoes.
+    :return: string array
+    """
+    if returns:
+        table = []
+
+    # checar se o banco de dados será utilizado, e, em caso positivo, fazer o seu setup.
+    using_database = False
+    if(connection is not None):
+        insert_query = """
+            INSERT INTO processo (numero_tjmg, acordao, ementa, sumula) VALUES 
+            (%s, %s, %s, %s)
+            """
+        using_database = True
+
+    for numproc in numprocs:
+        sleep(delay)
+        numproc_clean = numproc.replace("-", "").replace(".", "").replace("/", "")
+        try:
+
+            data, acordao = get_data_from_numproc(numproc, dir)
+            if data is None:
+                continue
+
+            # checar se a ementa não passou do limite de caracteres
+            if len(acordao) < lowerbound or len(acordao) > upperbound:
+                print("nao passou :c")
+                continue
+
+            # fazer a insercao dos dados no banco de dados, se possível
+            if using_database:
+                print("usando bd")
+                try:
+                    cursor.execute(insert_query, data)
+                    connection.commit()
+                except mysql.connector.Error:
+                    print("error inserting into table""")
+
+            # colocar as informacoes na tabela, se possível
+            if returns:
+                print("concatenando à tabela")
+                table.append(data)
+        except Exception as e:
+            print(e)
+            continue
+    # retornar, se possível
+    if returns:
+        return table
+
+
+def get_processo_table_essentials_file(file, connection, cursor, dir=getcwd() + "/processos", lowerbound = 700*5.5, upperbound = 4000*5.5, delay=0.15):
+
+    insert_query = """
+            INSERT INTO processo (numero_tjmg, acordao, ementa, sumula) VALUES 
+            (%s, %s, %s, %s)
+            """
+
+    # get numprocs from file
+    with open(file, "r+") as f:
+        numeros = [numero.strip("\n") for numero in f.readlines()]
+
+    failed_requests = 0
+    for numero in numeros:
+
+        if failed_requests > 9:
+            input("Houveram mais do que 9 requisicões falhadas. Verifique sua conexão com a internet e pressione enter para continuar")
+            failed_requests = 0
+
+        # pegar as infos
+        data, acordao = get_data_from_numproc(numero, dir)
+        if data is None:
+            failed_requests += 1
+            continue
+
+        # checar se a ementa não passou do limite de caracteres
+        if len(acordao) < lowerbound or len(acordao) > upperbound:
+            print("nao passou :c")
+            continue
+
+        print("usando bd")
+        try:
+            cursor.execute(insert_query, data)
+            connection.commit()
+        except mysql.connector.Error:
+            failed_requests += 1
+            print("error inserting into table")
+        finally:
+            remove_first_line(file)
