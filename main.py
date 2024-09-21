7fc7fd2cb7e0caf43197addc3a9e2a3208f36f1a
wendelfrota <wendelalves898@gmail.com>
2024-09-20 22:15:13 -0300

    fix: class import

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from database import load_env_vars, connect_to_database
-from tjmg_scraper import process_scraper as sc
+from tjmg_scraper.Scraper import Scraper
 
 
 def main():
@@ -13,7 +13,7 @@ def main():
     if not os.path.exists(path):
         os.makedirs(os.path.join(path, 'temp'))
 
-    sc.get_processo_table_essentials_file(
+    Scraper.get_processo_table_essentials_file(
         'test.txt',
         connection=connector,
         cursor=cursor,
