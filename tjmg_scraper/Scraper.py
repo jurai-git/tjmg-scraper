757c25028c16ef489cf42f1b26eddd916eb95738
wendelfrota <wendelalves898@gmail.com>
2024-09-20 21:51:42 -0300

    add: get_data_from_numproc method

--- tjmg_scraper/Scraper.py
+++ tjmg_scraper/Scraper.py
@@ -1,5 +1,7 @@
 import os
 import re
+from typing import Optional
+
 import fitz
 import mysql.connector as mysql
 import requests
@@ -49,3 +51,56 @@ class Scraper:
         with open(path, 'wb') as file:
             file.write(response.content)
         sleep(0.05)
+
+    def extract_text_from_pdf(self, pdf_path: str) -> Optional[str]:
+        try:
+            acordao_doc = fitz.open(pdf_path)
+        except Exception:
+            return None
+
+        acordao_txt = ''
+        for pagen in range(acordao_doc.page_count):
+            page = acordao_doc.load_page(pagen)
+            acordao_txt += page.get_text()
+
+        return acordao_txt
+
+    @staticmethod
+    def get_data_from_numproc(numproc, path):
+        data = [numproc]
+        pdf_path = os.path.join(path, 'temp', 'acordao.pdf')
+
+        try:
+            Scraper.get_inteiro_teor(
+                numproc,
+                os.path.join(path, 'temp'),
+                filename='acordao'
+            )
+        except Exception:
+            return None, None
+
+        acordao_txt = Scraper().extract_text_from_pdf(pdf_path)
+
+        if acordao_txt is None:
+            return None, None
+
+        cleaned_data = Scraper.clean_data(acordao_txt)
+        data.extend(cleaned_data)
+        return data, cleaned_data[0]
+
+    def clean_data(self, acordao_txt):
+        pattern_list = [r'^.*?EMENTA:', r'(?<=\n)\d+(?=\n)', r'Tribunal de Justiça de Minas Gerais\n']
+        data_pdf = re.sub('|'.join(pattern_list), '', acordao_txt, flags=re.DOTALL)
+
+        ementa = re.sub(r'(.*?)A\s+C\s+Ó\s+R\s+D\s+Ã\s+O.*', r'\1', data_pdf, flags=re.DOTALL)
+        ementa = ' '.join(ementa.split('\n\n')[:-1]) if ementa.split('\n\n') != 1 else ementa
+
+        acordao = re.sub(r'.*?V\sO\sT\sO\s+(.*?)SÚMULA.*', r'\1', data_pdf, flags=re.DOTALL)
+        sumula = re.sub(r'.*?SÚMULA:+(.*?)', r'\1', data_pdf, flags=re.DOTALL)
+
+        ementa = re.sub(r'\s+', ' ', ementa)
+        acordao = re.sub(r'\s+', ' ', acordao)
+        sumula = re.sub(r'\s+', ' ', sumula)
+
+        return [acordao, ementa, sumula]
+
